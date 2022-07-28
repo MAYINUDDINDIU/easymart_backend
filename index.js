276caff1c4c7924ede4d7e5c46d3694d1079e05f
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-07-28 13:20:20 +0600

    cors, dotenv jwt installed

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const cors = require("cors");
 const app = express()
 const port = process.env.PORT || 5000;
-
-
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+//Middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome Our Ecommerce Project Easymart')

2ad8eb26c98493be6b8795287f1b1114b7de72b7
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-09-04 17:30:19 +0600

    payment getway added

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const productRouter = require("./routes/product.route");
 app.use(cors());
 app.use(express.json());
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dfcq8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
+
 async function run() {
   try {
     await client.connect();
@@ -217,6 +218,23 @@ async function run() {
     app.get("/reviews", async (req, res) => {
       const allReviews = await reviewCollection.find().toArray();
       res.send(allReviews);
+    });
+
+    //Creating Payment
+    app.post("/create_payment_intent", async (req, res) => {
+      const price = req.body.totalAmountToPay;
+
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
   } finally {
   }
